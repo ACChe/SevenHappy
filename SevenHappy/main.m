1278eaf567c40f0a5f6bd359a6dc764860355139
codepapa <cam.che@gmail.com>
2013-09-02 00:13:39 +0800

    test array

--- SevenHappy/main.m
+++ SevenHappy/main.m
@@ -10,5 +10,39 @@
 
 int main(int argc, const char * argv[])
 {
-    return NSApplicationMain(argc, argv);
+    @autoreleasepool
+    {
+        
+        //Read the data from external file
+        NSString *filePathName = @"/Users/Calileo/Dropbox/SevenHappy.txt";
+        NSError *fileError;
+        NSString *textFileContents = [NSString stringWithContentsOfFile:filePathName encoding:NSASCIIStringEncoding error:&fileError];
+        if (fileError.code == 0)
+        {
+//            NSLog(@"textfile.txt contents: %@", textFileContents);
+        }
+        else
+        {
+            NSLog(@"error(%ld) : %@", fileError.code, fileError.description);
+        }
+        
+        if (textFileContents != NULL)
+        {
+            NSArray *totalStringList = [textFileContents componentsSeparatedByString:@"\n"];
+            NSLog(@"result is %@", totalStringList);
+            NSLog(@"total number is : %ld", [totalStringList count]);
+            NSArray *tempStringArray = [totalStringList objectAtIndex:0];
+            NSLog(@"one of the list is : %@", tempStringArray);
+            
+            NSMutableArray *targetMutableArray = [[NSMutableArray alloc] init];
+            
+            [tempStringArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+//                [targetMutableArray addObject:[[obj objectAtIndex:idx] componentsSeparatedByString:@","]];
+                NSLog(@"object(%lu)'s description is %@", idx, [obj description]);
+            }];
+        }
+        
+    }
+//    return NSApplicationMain(argc, argv);
+    return 0;
 }
