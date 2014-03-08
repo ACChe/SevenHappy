26f3d1201a77749f2505ba1cb04e23f7d899bdad
AC <ac.chen@afmobigroup.com>
2014-03-08 13:40:01 +0800

    modify some incorrect code.

--- SevenHappy/Store.m
+++ SevenHappy/Store.m
@@ -10,8 +10,8 @@
 #import "Numbers.h"
 
 @implementation Store
-@synthesize operatedNumbers = _operatedNumbers;
-@synthesize fileredNumbers = _fileredNumbers;
+
+
 + (instancetype)store
 {
     return [[self alloc] init];
@@ -48,21 +48,27 @@
     {
         NSArray *totalStringList = [textFileContents componentsSeparatedByString:@"\n"];
 //        NSLog(@"result is %@", totalStringList);
-//        NSLog(@"total number is : %ld", [totalStringList count]);
-//        NSArray *tempStringArray = [totalStringList objectAtIndex:0];
-//        NSLog(@"one of the list is : %@", tempStringArray);
+        NSLog(@"totalStringList's count is : %ld", [totalStringList count]);
+
+//        _numbers = [NSArray arrayWithArray:totalStringList];
+        _numbers = [self groupTheSourceDada:totalStringList];
+        NSLog(@"_number's is : %@", _numbers);
         
-        NSMutableArray *targetMutableArray = [[NSMutableArray alloc] init];
-        
-        [totalStringList enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-            [targetMutableArray addObject:[obj componentsSeparatedByString:@","]];
-            NSLog(@"here (%lu) is %@ ", (unsigned long)idx, [targetMutableArray objectAtIndex:idx]);
-        }];
-        
-        _numbers = [NSArray arrayWithArray:targetMutableArray];
     }
 }
 
+#pragma mark - Group the data source to 7 per 1
+- (NSArray *)groupTheSourceDada:(NSArray *) originData{
+    NSMutableArray * resultArray = [[NSMutableArray alloc] init];
+    [originData enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+         NSArray * tempArray = [obj componentsSeparatedByString:@","];
+        [resultArray addObject:tempArray];
+    }];
+    return  [resultArray copy];
+}
+
+
+#pragma mark - Save the result to local file
 - (void) saveResultToHistory:(NSArray *)aArray
 {
 //    NSBundle *mainBundle = [NSBundle mainBundle];
@@ -75,46 +81,30 @@
     [aArray writeToFile:histroyFilePahtName atomically:YES];
     
 }
-- (NSArray *)filterNumbersByNoDuplicate:(NSArray *)originalArray
-{
-    NSMutableArray * tempMutableArray = [NSMutableArray arrayWithArray:originalArray];
-//    [tempMutableArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-//    }]
-    NSLog(@"there are %lu 's numbers", tempMutableArray.count);
-    for (int i = 0; i < tempMutableArray.count - 1; i++)
-    {
-        for (int j = i + 1; j < tempMutableArray.count; j++)
-        {
-            if ([[tempMutableArray objectAtIndex:i] isEqualToArray:[tempMutableArray objectAtIndex:j]])
-            {
-                NSLog(@"left[%d] : %@ is equal to right[%d] : %@", i, [tempMutableArray objectAtIndex:i], j, [tempMutableArray objectAtIndex:j]);
-                [tempMutableArray removeObjectAtIndex:j];
-            }
-        }
-    }
-    NSLog(@"Total amount is = %lu", tempMutableArray.count);
-    return [NSArray arrayWithArray:tempMutableArray];
-}
+
+#pragma mark - method of distiguish the array is the same
 
 - (BOOL)isArray:(NSArray *)aArray existInArrayPool:(NSArray *)poolArray
 {
-    for (int i = 0; i < aArray.count; i++)
-    {
-        for (int j = 0; j < poolArray.count; j++)
-        {
-            if([[aArray objectAtIndex:i] isEqualToArray:[poolArray objectAtIndex:j]])
-            {
-                return YES;
+    NSLog(@"count = %ld", aArray.count);
+    __block BOOL result = NO;
+
+    [poolArray enumerateObjectsUsingBlock:^(id obj_out, NSUInteger idx_out, BOOL *stop_out) {
+        [aArray enumerateObjectsUsingBlock:^(id obj_in, NSUInteger idx_in, BOOL *stop_in) {
+            if ([aArray[idx_in] isEqualTo:obj_out]){
+                result = YES;
+                *stop_out = YES;
             }
-        }
-    }
-    return NO;
+        }];
+    }];
+    NSLog(@"Result is %i", result);
+    return result;
 }
 
-
+#pragma mark - Random 1 - 30 number
 - (NSArray *) gem30RandomNumbers
 {
-    NSArray *temp = [NSArray arrayWithObjects:@"1",@"2", @"3",@"4", @"5",@"6", @"7", @"8", @"9", @"10", @"11", @"12", @"13", @"14", @"15", @"16", @"17", @"18", @"19", @"20", @"21", @"22", @"23", @"24", @"25", @"26", @"27", @"28", @"29", @"30", nil];
+    NSArray *temp = [NSArray arrayWithObjects:@"1", @"2", @"3",@"4", @"5",@"6", @"7", @"8", @"9", @"10", @"11", @"12", @"13", @"14", @"15", @"16", @"17", @"18", @"19", @"20", @"21", @"22", @"23", @"24", @"25", @"26", @"27", @"28", @"29", @"30", nil];
     NSMutableArray *tempArray = [[NSMutableArray alloc]initWithArray:temp];
     NSMutableArray*resultArray = [[NSMutableArray alloc]init];
     int i;
@@ -129,31 +119,20 @@
     return [NSArray arrayWithArray:[self splitArray:resultArray PerCount:7]];
 }
 
-
-- (NSArray *) gemFiveRandomNumbers
+#pragma mark - Random five arrary with scope (1-30)
+- (NSArray *) gemFiveRandomNumbersoutOfScope:(NSArray *)numberPool
 {
     NSArray * tempArray = [[NSArray alloc] init];
     do
     {
         tempArray = [self gem30RandomNumbers];
     }
-    while ([self isArray:tempArray existInArrayPool:_fileredNumbers]);
+    while ([self isArray:tempArray existInArrayPool:numberPool]);
     return tempArray;
 }
 
-#pragma Utili method
-- (BOOL) oneNumber:(int)aNumber isExistInPool:(NSArray *)thePool
-{
-    for (int i = 0; i < thePool.count; i++)
-    {
-        if ([NSNumber numberWithInt:aNumber] == [thePool objectAtIndex:i])
-        {
-            return YES;
-        }
-    }
-    return NO;
-}
 
+#pragma mark - split the array to one set of seven
 - (NSArray *) splitArray:(NSArray *)aArray PerCount:(int)aCount
 {
     NSMutableArray * tempArray = [[NSMutableArray alloc] init];
